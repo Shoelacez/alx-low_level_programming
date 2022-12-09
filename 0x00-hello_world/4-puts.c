2455add48efc95d5cef660914ecff319e4ccf258
1300Hrs <alphayojoseph2@gmail.com>
2022-12-09 07:35:29 -0800

    Corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,9 @@
 
 /**
  *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 
 int main(void)
 
