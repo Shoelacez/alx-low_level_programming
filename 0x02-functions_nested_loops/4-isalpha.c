89a38fc1d981f3676a44e6cad37f9af2a7063c7f
1300Hrs <alphayojoseph2@gmail.com>
2022-12-13 09:19:23 -0800

    7

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,11 +3,11 @@
 /**
  * _isalpha - checks if character is a letter
  * Return: 1 is c if true else 0 
- */
+*/
 
 int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122 && c >= 65 && c >= 90)
-			return (1);
+		return (1);
 	return (0);
 }
