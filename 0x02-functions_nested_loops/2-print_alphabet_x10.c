849fc00827af474174c3944d1406318770b022b3
1300Hrs <alphayojoseph2@gmail.com>
2022-12-13 06:14:23 -0800

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- *print_alphabet_x10 - function that will print the alphabet 10 times 
+ * print_alphabet_x10 - function that will print the alphabet 10 times
  */
 
 void print_alphabet_x10(void)
@@ -11,9 +11,7 @@ void print_alphabet_x10(void)
 	for (line = 0; line <= 9; ++line)
 	{
 		for (ch = 'a'; ch <= 'z'; ++ch)
-		{
 			_putchar(ch);
-		}
 		_putchar('\n');
 	}
 }
